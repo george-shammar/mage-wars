4bb06400c5b639cd0528f231fed0784c17c182f2
george-shammar <george.gbenle@outlook.com>
2021-12-03 14:03:53 +0000

    Get rid on unwanted component

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './Home';
-import App from './App';
 import MintHero from './MintHero';
 
 
@@ -10,7 +9,6 @@ const Routes = () => (
     <div className="route">
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/App" component={App} />
         <Route exact path="/MintHero" component={MintHero} />
         
       </Switch>
