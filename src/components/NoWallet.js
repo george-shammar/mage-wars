f430a0c230395bc88c572310c963c21f1440f6cb
george-shammar <george.gbenle@outlook.com>
2021-12-03 12:33:32 +0000

    Style no wallet page

--- src/components/NoWallet.js
+++ src/components/NoWallet.js
@@ -6,18 +6,17 @@ export const NoWallet = () => {
         <div className="d-flex main flex-column justify-content-between align-text-center pt-2 px-4">
         <h1 className="pink azania py-2">Mage Wars</h1> 
         <p className="white">No Ethereum wallet was detected. Connect to play Mage Wars.</p>
-          <div className="px-3 border-0 rounded-pill pt-2 wallet white">
-            <p className="white">
-              Install{" "}
-              <a
+        <a
                 href="http://metamask.io"
                 target="_blank"
                 rel="noopener noreferrer"
-              >
+              > <div className="px-3 border-0 rounded-pill py-1 wallet white">
+            <p className="white">
+              Install{" "}
+             
                 MetaMask
-              </a>
             </p>
-          </div>
+          </div> </a>
         </div>
         {/* <p className="no-wallet">No Ethereum wallet was detected.</p> */}
         </div>
