fc5a2173171f0468faea3a57126f13cb5c15a11d
george-shammar <george.gbenle@outlook.com>
2021-12-03 16:29:17 +0000

    Create mint

--- src/components/MintHero.js
+++ src/components/MintHero.js
@@ -64,17 +64,20 @@ const Nav = () => {
       <div className="border card rounded shadow-lg mt-5">
         <img alt="logo" src={question} className="mx-2 my-2" />
       </div>
-      <input placeholder="Name your Hero"></input>
-      <div className="white">
-        <p>DNA: To be revealed</p>
-        <p>Rarity: To be revealed</p>
-        <p>Description/Powers: To be reveealed</p>
+      <div className="border">
+        <input className="name py-1 white" placeholder="Name your Hero"></input>
+        <div className="white mt-3">
+          <p>DNA: To be revealed</p>
+          <p>Rarity: To be revealed</p>
+          <p>Description/Powers: To be reveealed</p>
+        </div>
+          
+          <button className="py-2 submit white">
+              Mint Hero
+            </button>
+        </div>
       </div>
-        
-        <button className="mb-3 py-2 submit">
-            Mint Hero
-          </button>
-      </div>
+      
     
     <div>
       <p>Reveal minted Hero</p>
