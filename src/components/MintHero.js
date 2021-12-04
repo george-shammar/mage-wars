b304c9e57a3643a6ef0e3ef8a7ab40d802286939
george-shammar <george.gbenle@outlook.com>
2021-12-04 20:02:56 +0000

    Update minter form

--- src/components/MintHero.js
+++ src/components/MintHero.js
@@ -153,7 +153,7 @@ const MintHero = () => {
           <p>Description/Powers: To be reveealed</p>
         </div>
           
-          <button className="py-2 submit white">
+          <button className="py-2 submit white" onClick={mintHero}>
               Mint Hero
             </button>
         </div>
