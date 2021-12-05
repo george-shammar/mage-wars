49b8212f6a929b614d4b7ab82efcfb375e960632
george-shammar <george.gbenle@outlook.com>
2021-12-05 14:51:58 +0000

    Center image

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
         <div className="home">
             <div className="center">
                 <h1 className="pink azania py-2">Mage Wars</h1>
-                <p className="white">Ready to smoke the Dragon?</p>
+                <p className="white">Ready to smoke the Dragon? Mint a Mage to fight the Dragon.</p>
                 <Link to="/MintHero"><button className="px-3 border-0 rounded-pill p-2 wallet white">Mint A Hero To Play</button></Link>
              
             </div>
