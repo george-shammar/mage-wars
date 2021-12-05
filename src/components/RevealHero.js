350b360759060720c4218414cc5a724ede6b4612
george-shammar <george.gbenle@outlook.com>
2021-12-05 13:08:18 +0000

    Update RevealHero component

--- src/components/RevealHero.js
+++ src/components/RevealHero.js
@@ -2,7 +2,26 @@ import "../styles/RevealHero.css"
 const RevealHero = () => {
     return (
         <div>
-            <p>Hero goes here</p>
+            <div className="mint">
+                <div className="border card rounded shadow-lg mt-3">
+                    <img alt="logo" src={question} className="unknown"/>
+                </div>
+                <div className="border">
+                    <input 
+                    className="name py-1 white" 
+                    placeholder="Name your Hero"
+                    required
+                    onChange={e => updateFormInput({...formInput, name: e.target.value})}  
+                    />
+                    <div className="white mt-3">
+                    <p>ID: To be revealed</p>
+                    <p>DNA: To be revealed</p>
+                    <p>Rarity: To be revealed</p>
+                    <p>Level: To be revealed</p>
+                    <p>Description/Powers: To be reveealed</p>
+                    </div>
+                </div>
+      </div>
 
             <div className="mint-reveal">
                 <a href="https://george-shammar.github.io/Legend_of_the_Seeker/"><button className="white rounded my-5 py-2 submit white">Play Now</button></a>
